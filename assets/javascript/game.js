38d26c2375678e90c1509f5fe5bddf526ad3bd81
Dmitrii Zakharov <dzakharov@splunk.com>
2018-08-29 18:27:38 -0700

    Implemented assigning values to crystals and game logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,37 +11,33 @@ function reset() {
 //document.onkeyup = function(event) {
 
     var targetNumber = Math.floor(Math.random()*100)+19;
-    alert(targetNumber);
 
     $("#random-number").text(targetNumber);
 
-  var counter = 0;
+    var counter = 0;
 
-  // Now for the hard part. Creating multiple crystals each with their own unique number value.
+    // Now for the hard part. Creating multiple crystals each with their own unique number value.
+    // We begin by expanding our array to include four options.
+    var numberOptions = [10, 5, 3, 7];
+    var crystalValue = [];
 
-  // We begin by expanding our array to include four options.
-  var numberOptions = [10, 5, 3, 7];
-
-  // Next we create a for loop to create crystals for every numberOption.
-  for (var i = 0; i < numberOptions.length; i++) {
-
-    // For each iteration, we will create an imageCrystal
-    var imageCrystal = $("<img>");
-
-    // First each crystal will be given the class ".crystal-image".
-    // This will allow the CSS to take effect.
-    imageCrystal.addClass("crystal-image");
-
-    // Each imageCrystal will be given a src link to the crystal image
-    imageCrystal.attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
-
-    // Each imageCrystal will be given a data attribute called data-crystalValue.
-    // This data attribute will be set equal to the array value.
-    imageCrystal.attr("data-crystalvalue", numberOptions[i]);
-
-    // Lastly, each crystal image (with all it classes and attributes) will get added to the page.
-    $("#crystals").append(imageCrystal);
-  }
+    // Next we create a for loop to create crystals for every numberOption.
+    for (var i = 0; i < numberOptions.length; i++) {        
+        var option = Math.floor(Math.random()*4);
+        alert(option);    
+        crystalValue[i] = numberOptions[option];
+        switch (i) {
+            case 0:
+                $("#crystal0").attr("data-crystalvalue", crystalValue[0]);                
+            case 1:
+                $("#crystal1").attr("data-crystalvalue", crystalValue[1]);
+            case 2:
+                $("#crystal2").attr("data-crystalvalue", crystalValue[2]);
+            case 3:
+                $("#crystal3").attr("data-crystalvalue", crystalValue[3]);
+        }
+        alert("Set crystal #"+i+" to "+crystalValue[i]);
+    }
 
   // This time, our click event applies to every single crystal on the page. Not just one.
   $(".crystal-image").on("click", function() {
@@ -73,6 +69,7 @@ function reset() {
 }
 
 });
+
 
 
 
